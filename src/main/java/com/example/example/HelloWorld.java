fd7978fcb50b61694a89863abe4998e46a3630c6
sunny134bhartiya <sunny134bhartiya@gmail.com>
2021-05-27 06:59:10 +0000

    1 commit

---
+++ src/main/java/com/example/example/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.example.example;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
+public class HelloWorld {
+@RequestMapping("/hello")
+@ResponseBody
+public String sayHello() {
+return "Hello World sunny";
+}
+}
